baf3465411e867b753e8fe94f60e762411be31e7
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2025-09-23 15:48:34 -0300

    iniciando o projeto com a estrutura basica

---
+++ src/main/java/br/com/medtech/ms_medtech/MsMedtechApplication.java
@@ -0,0 +1,13 @@
+package br.com.medtech.ms_medtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMedtechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMedtechApplication.class, args);
+	}
+
+}
