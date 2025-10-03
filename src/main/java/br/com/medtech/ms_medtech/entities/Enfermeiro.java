1a78804165f8d37fce51c990c7ffa261f6941232
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2025-10-03 17:04:41 -0300

    fazendo ajustes para salvar o campo coren

--- src/main/java/br/com/medtech/ms_medtech/entities/Enfermeiro.java
+++ src/main/java/br/com/medtech/ms_medtech/entities/Enfermeiro.java
@@ -2,18 +2,21 @@ package br.com.medtech.ms_medtech.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "tb_enfermeiro")
 public final class Enfermeiro extends Usuario{
     private String coren;
 
-    public Enfermeiro() {
-    }
+    public Enfermeiro() {}
 
     public Enfermeiro(UUID id, String nome, LocalDate dataNascimento, LocalDateTime dataCadastro, String telefone, boolean enabled, Login login, Role role, String coren) {
         super(id, nome, dataNascimento, dataCadastro, telefone, enabled, login, role);
